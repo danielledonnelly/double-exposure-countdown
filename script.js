289e1d3439d8e3f52b3498218edf27416a1d3347
danielledonnelly <daniellemarydonnelly@gmail.com>
2024-06-16 10:51:43 -0230

    first timer fix

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
 // Reveal Livestream Countdown
-var revealCountdownDate = new Date(); 
-var currentDay = revealCountdownDate.getDay(); 
-var targetDay = 4; // 
-var targetTime = 13.5 * 60 * 60 * 1000;
-
-var daysToAdd = targetDay - currentDay >= 0 ? targetDay - currentDay : 7 - currentDay + targetDay;
-revealCountdownDate.setDate(revealCountdownDate.getDate() + daysToAdd);
-revealCountdownDate.setHours(13, 30, 0, 0);
-
-var revealCountdown = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = revealCountdownDate - now;
-
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    var countdownText = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(revealCountdown);
-        countdownText = "0d 0h 0m 0s";
-    }
-
-    document.getElementById("reveal-countdown").innerHTML = countdownText;
-}, 1000);
+document.getElementById("reveal-countdown").innerHTML = "0d 0h 0m 0s";
+// The livestream is already out but the way I had this set up originally made it keep resetting so it's just permanently set to 0 now.
 
 // Game Release Countdown
 var gameCountdownDate = new Date("October 29, 2024 00:00:00").getTime();
